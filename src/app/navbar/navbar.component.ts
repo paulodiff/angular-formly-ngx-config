c39b133c3318cd25d16a50b4ec0c635201c6558a
paulodiff <paulo.difficiliora@gmail.com>
2021-02-22 20:52:01 +0100

    2 files added

---
+++ src/app/navbar/navbar.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+
+@Component({
+  selector: 'app-navbar',
+  templateUrl: './navbar.component.html',
+  styleUrls: ['./navbar.component.css']
+})
+export class NavBarComponent implements OnInit {
+
+  loading: boolean;
+  
+
+  constructor(
+    // private loaderService: LoaderService
+    ) {
+
+    /*
+    this.loaderService.isLoading.subscribe((v) => {
+      console.log(v);
+      this.loading = v;
+    });
+
+    */
+
+  }
+  ngOnInit() {
+  }
+
+  
+
+  toggleNavbar() {
+    this.navbarOpen = !this.navbarOpen;
+  }
+
+}
